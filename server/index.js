4eeadd3e634edbfabfed578803b96d9bf961e1f8
awallemo <awallemo_98@hotmail.com>
2024-11-30 13:04:14 +0100

    feat: add mongodb

--- server/index.js
+++ server/index.js
@@ -19,9 +19,9 @@ app.use(cors({
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect('mongodb+srv://taskManagerDbUser:taskManagerDbUserPassword@taskmanager.osbr5.mongodb.net/')
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch(err => {console.log(process.env.MONGODB_URI); console.error('MongoDB connection error:', err)});
 
 // Routes
 app.use('/api/auth', authRoutes);
